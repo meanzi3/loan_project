e9ea3cf783fed2e2f61db17b90dd7157138bcafd
Minji <minjichoe0326@gmail.com>
2023-04-01 20:10:06 +0900

    Initial commit

---
+++ src/main/java/com/example/loan_project/LoanProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.loan_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoanProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/loan_project/LoanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loan_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
